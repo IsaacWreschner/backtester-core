2857430518841fbbac7b0a78bab93702f30e669b
Isaac Werchner <51373421+IsaacWreschner@users.noreply.github.com>
2025-06-05 10:05:00 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,2 @@
-# -- TMP --
-from sys import path
-import os
-path.append(os.path.dirname(os.path.realpath(__file__)))
-# -- END TMP --
-from models import Parameters, CustomParametersDefinition, Position, Timeframe, Symbol, TestSandbox
+from models import Position, Timeframe, Symbol
 from session import Session
